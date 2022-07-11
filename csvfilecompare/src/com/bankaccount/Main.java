3a5df9da48f1da6800de9a9b4da3c09b2c569617
zywail <90777322+zywail@users.noreply.github.com>
2022-07-11 22:01:15 +0800

    + implementation of add csv file

--- csvfilecompare/src/com/bankaccount/Main.java
+++ csvfilecompare/src/com/bankaccount/Main.java
@@ -68,5 +68,26 @@ public class Main {
             i++;
         }
         System.out.println(outputaccount);
+        
+        File csvFile = new File("output.csv");
+        FileWriter fileWriter = new FileWriter(csvFile);
+
+        if (true) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < outputaccount.size(); j++) {
+//                line.append("\"");
+                line.append(outputaccount.get(j)
+//                        .replaceAll("\"","\"\"")
+                );
+                line.append("\n");
+//                line.append("\"");
+                if (j != outputaccount.size() - 1) {
+                   continue;
+                }
+            }
+            line.append("\n");
+            fileWriter.write(line.toString());
+        }
+        fileWriter.close();
     }
 }
